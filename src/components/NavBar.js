14edd7257a60eb474c27b97533e69e7b46181a0d
Arian Perez <arianfelipeperez@gmail.com>
2024-09-28 10:37:27 -0300

    actualizacion navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,54 +1,54 @@
-import React, {useState}  from "react";
+import React, { useState } from "react";
 import './NavBar.css'
 import { Link } from 'react-router-dom';
+
 const Navbar = () => {
 
     // to change burger classes
-    const [burger_class, setBurgerClass] = useState("burger-bar unclicked")
-    const [menu_class, setMenuClass] = useState("menu hidden")
-    const [isMenuClicked, setIsMenuClicked] = useState(false)
+    const [burger_class, setBurgerClass] = useState("burger-bar unclicked");
+    const [menu_class, setMenuClass] = useState("menu hidden");
+    const [isMenuClicked, setIsMenuClicked] = useState(false);
 
     // toggle burger menu change
-
-   
     const updateMenu = () => {
-        if(!isMenuClicked) {
-            setBurgerClass("burger-bar clicked")
-            setMenuClass("menu visible")
+        if (!isMenuClicked) {
+            setBurgerClass("burger-bar clicked");
+            setMenuClass("menu visible");
+        } else {
+            setBurgerClass("burger-bar unclicked");
+            setMenuClass("menu hidden");
         }
-        else  {
-            setBurgerClass("burger-bar unclicked")
-            setMenuClass("menu hidden")
-        }
-        setIsMenuClicked(!isMenuClicked)
-    }
+        setIsMenuClicked(!isMenuClicked);
+    };
 
-
-
-    return(
+    return (
         <div className="divnavbar">
             <nav>
                 <div className="burger-menu" onClick={updateMenu}>
-                    <div className={burger_class} ></div>
-                    <div className={burger_class} ></div>
-                    <div className={burger_class} ></div>
+                    <div className={burger_class}></div>
+                    <div className={burger_class}></div>
+                    <div className={burger_class}></div>
                 </div>
             </nav>
-                
+
             <div className={menu_class}>
-            <ul className="listapadre">
-                    <li className="listahija"><Link to="/"onClick={isMenuClicked}>- Inicio</Link></li>
-                    <li className="listahija"><Link to="/about"onClick={isMenuClicked}>- categoria</Link></li>
-                    <li className="listahija"><Link to="/services"onClick={isMenuClicked}>- Categoria</Link></li>
-                    <li className="listahija"><Link to="/Contacto"onClick={isMenuClicked}>- Contacto</Link></li>
+                <ul className="listapadre">
+                    <li className="listahija">
+                        <Link to="/" onClick={updateMenu}>- Inicio</Link>
+                    </li>
+                    <li className="listahija">
+                        <Link to="/about" onClick={updateMenu}>- Categoria</Link>
+                    </li>
+                    <li className="listahija">
+                        <Link to="/services" onClick={updateMenu}>- Categoria</Link>
+                    </li>
+                    <li className="listahija">
+                        <Link to="/Contacto" onClick={updateMenu}>- Contacto</Link>
+                    </li>
                 </ul>
-
-
-
             </div>
-            
         </div>
-    )
-}
+    );
+};
 
-export default Navbar
+export default Navbar;
